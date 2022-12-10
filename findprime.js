c29247442557b78c5d27960603b7c196d0888d9e
sudhansu nayak <nayaksd2001@gmail.com>
2022-12-10 14:26:36 +0530

    prime code fix

--- findprime.js
+++ findprime.js
@@ -1,7 +1,19 @@
-num = 13;
+function prime(num){
 count = 0;
-for(let i = 0<=i<num;i++){
-if(num%i==0){
-count++;}}
-if(count=0){
-console.log("It is prime");
+  for(let i =2;i<=num/2;i++){
+    if(num%i==0){
+      count++;
+  }
+}
+ if(count==0){
+  return true;
+}else{
+ return false;
+}
+}
+let ans=prime(13);
+ if (ans==true){
+   console.log("Prime");
+ }else{
+   console.log("not Prime");
+}
